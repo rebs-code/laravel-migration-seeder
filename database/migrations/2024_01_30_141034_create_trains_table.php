74eac0eb3ccd0ab2cee8ed006e1edaf91c6cb57d
rebs-code <jack.rebussi@gmail.com>
2024-01-30 15:58:27 +0100

    add: instructions to migration file to create table

--- database/migrations/2024_01_30_141034_create_trains_table.php
+++ database/migrations/2024_01_30_141034_create_trains_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 100);
+            $table->string('stazione_di_arrivo', 100);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno', 20);
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->tinyInteger('in_orario')->unsigned()->default(1);
+            $table->tinyInteger('cancellato')->unsigned()->default(0);
+
             $table->timestamps();
         });
     }
